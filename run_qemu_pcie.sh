df873afdd23e7174578ae1c6f9a18946dab56f22
Bigyellow722 <lhkwqy@qq.com>
2023-04-11 21:10:47 +0800

    [qemu-script] support gdb
    Signed-off-by: Bigyellow722 <lhkwqy@qq.com>

--- run_qemu_pcie.sh
+++ run_qemu_pcie.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-qemu-system-x86_64 -s  \
-    -cpu host   \
+qemu-system-x86_64 -s -S \
     -machine q35  \
-    -enable-kvm \
     -kernel /home/wqy/code/linux-6.1.11/arch/x86/boot/bzImage  \
     -initrd initramfs.cpio.gz \
     -nographic \
-    -append "console=ttyS0 cma=1M@0" \
+    -append "console=ttyS0 nokaslr cma=1M@0" \
     -device pci-bridge,id=bridge0,chassis_nr=1 \
     -device virtio-scsi-pci,id=scsi0,bus=bridge0,addr=0x3 \
     -device pci-bridge,id=bridge1,chassis_nr=2 \
--- run_qemu_usb.sh
+++ run_qemu_usb.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-qemu-system-x86_64 -s  \
-		   -cpu host   \
+qemu-system-x86_64 -s -S \
 		   -machine q35  \
-		   -enable-kvm \
 		   -kernel /home/wqy/code/linux-6.1.11/arch/x86/boot/bzImage  \
 		   -initrd initramfs.cpio.gz \
 		   -nographic \
-		   -append "console=ttyS0 cma=1M@0" \
+		   -append "console=ttyS0 nokaslr cma=1M@0" \
 		   -drive if=none,id=stick,format=raw,file=usb-disk.img \
 		   -device nec-usb-xhci,id=xhci                              \
 		   -device usb-storage,bus=xhci.0,drive=stick  \
